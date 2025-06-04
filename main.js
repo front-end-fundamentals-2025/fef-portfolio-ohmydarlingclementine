f2db63383fce65135a11d8881093264da56e7685
ohmydarlingclementine <linaorundas70@gmail.com>
2025-06-04 21:05:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-const aboutBtn = document.getElementById('about-btn');
-const message = document.getElementById('message');
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("about-btn");
+  const message = document.getElementById("message");
 
-aboutBtn.addEventListener('click', () => {
-  message.textContent = "Hey there!!";   // setting the message text here
-  message.style.display = "block"; // show the message here
+  button.addEventListener("click", function () {
+    message.textContent = "Hey there you!!";
+    message.style.display = "block";
+  });
 });
