52cfcff7dcabf3254d4e0c1f212f4ca3709bf3c5
ohmydarlingclementine <linaorundas70@gmail.com>
2025-06-05 18:14:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
+
 document.addEventListener("DOMContentLoaded", function () {
   const button = document.getElementById("about-btn");
+  const message = document.getElementById("message");
 
   button.addEventListener("click", function () {
-    /* change button color */
-    button.style.backgroundColor = "##fcb1e7"; // light pink
-    button.style.color = "#ffffff"; // white text
+    // change button background and text color
+    button.style.backgroundColor = "##fcb1e7"; //light pink
+    button.style.color = "#fff"; //white text
+
+    // show a message below the button
+    message.style.display = "block";
+    message.textContent = "Hey! You clicked the About Me button!";
   });
 });
-
