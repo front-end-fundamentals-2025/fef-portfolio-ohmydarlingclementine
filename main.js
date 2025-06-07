094ceb4ef5e0ac134c546f0f11857ac271e5267d
ohmydarlingclementine <linaorundas70@gmail.com>
2025-06-07 19:29:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const button = document.getElementById("about-btn");
 
   button.addEventListener("click", function () {
-    // Add a class to change its color
-    button.classList.add("clicked");
+    button.style.backgroundColor = "#fcb1e7";
+    button.style.color = "#fff";
   });
 });
-
