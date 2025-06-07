0416271275716e17314fee1b4934ce8cb50fa401
ohmydarlingclementine <linaorundas70@gmail.com>
2025-06-07 19:34:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const button = document.getElementById("about-btn");
 
   button.addEventListener("click", function () {
-    button.style.backgroundColor = "#fcb1e7";
-    button.style.color = "#fff";
+    // toggle the clicked class to change color
+    button.classList.toggle("clicked");
   });
 });
