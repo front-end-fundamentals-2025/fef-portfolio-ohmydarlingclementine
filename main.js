2a93303116f9d320543ee83cbfe7ad92adba3aa7
ohmydarlingclementine <linaorundas70@gmail.com>
2025-06-07 19:11:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   const button = document.getElementById("about-btn");
-  const message = document.getElementById("message");
 
   button.addEventListener("click", function () {
-    // change button background and text color
-    button.style.backgroundColor = "#fcb1e7"; 
-    button.style.color = "#fff"; // white text
-
-    // show a message below the button
-    message.style.display = "block";
-    message.textContent = "Hey! You clicked the About Me button!";
+    // Add a class to change its color
+    button.classList.add("clicked");
   });
 });
+
